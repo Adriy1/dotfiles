9db5cabe9a7848f7191c441fa718837db10956af
Your Name <you@example.com>
2018-08-10 15:49:49 +0200

    ajout de l'install auto et fix install de oh my zsh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+if [ $(dpkg-query -W -f='${Status}' vim 2>/dev/null | grep -c "ok installed") -eq 0 ];
+then
+  sudo apt-get install vim;
+fi
+
+if [ $(dpkg-query -W -f='${Status}' tmux 2>/dev/null | grep -c "ok installed") -eq 0 ];
+then
+  sudo apt-get install tmux;
+fi
+
+if [ $(dpkg-query -W -f='${Status}' zsh 2>/dev/null | grep -c "ok installed") -eq 0 ];
+then
+  sudo apt-get install zsh;
+fi
 
 mv tmux.conf ~/.tmux.conf
 mv vimrc ~/.vimrc
@@ -8,10 +22,10 @@ cd
 
 git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
 sh ~/.vim_runtime/install_awesome_vimrc.sh
-sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)" & wait
 
 mv ~/dotfiles/simple.zsh-theme ~/.oh-my-zsh/themes
 
 echo "chpwd() ls" >> ~/.zshrc
 
-rm -r ~/dotfiles
+rm -rf ~/dotfiles
